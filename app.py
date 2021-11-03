d400933644815465d6bd46e82349c357328ee801
Zabee52 <zabeee52@gmail.com>
2021-11-03 18:26:11 +0900

    first commit

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from googleapiclient.discovery import build
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 from pymongo import MongoClient
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
+import random
+import jwt
+import hashlib
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
+
+SECRET_KEY = 'SPARTA'
 
 client = MongoClient('localhost', 27017)
-db = client.dbteamproj
+db = client.youtuberandomplaylist
 
 api_key = 'AIzaSyDoc0GN-_nBuANpy8f893HDttg71tF5szs'
 youtube = build('youtube', 'v3', developerKey=api_key)
@@ -14,45 +23,161 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 # HTML을 주는 부분
 @app.route('/')
 def home():
-    return render_template('index.html')
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template('index.html', user_info=user_info)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("index", msg="로그인 정보가 존재하지 않습니다."))
 
 
-@app.route('/play')
+@app.route('/index')
+def index():
+    msg = request.args.get("msg")
+    return render_template('index.html', msg=msg)
+
+
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+
+@app.route('/agreement', methods=['GET'])
+def agreement():
+    msg = request.args.get("msg")
+    return render_template('agreement.html', msg=msg)
+
+
+@app.route('/sign_up', methods=['GET'])
+def sign():
+    msg = request.args.get("msg")
+    return render_template('sign_up.html', msg=msg)
+
+
+@app.route('/randomplaylist')
 def play():
-    return render_template("play.html")
+    token_receive = request.cookies.get('mytoken')
+    user_info = ''
+    login = ''
 
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
+    except jwt.ExpiredSignatureError:
+        login="로그인 시간이 만료되었습니다."
+    except jwt.exceptions.DecodeError:
+        login = "로그인 정보가 존재하지 않습니다."
 
-@app.route('/mypage')
-def mypage():
-    return render_template("mypage.html")
+    top_tags = list(db.tag.find({}, {'_id': False}).distinct('tag'))
+    random.shuffle(top_tags)
+    top_tags_response = top_tags[:3]
+
+    playlistId_receive = request.args.get('playlistId')
+
+    likes = list(db.followlist.find({'playlistId': playlistId_receive}, {'_id': False}))
+    likes_response = likes[:2]
+    likes_cnt = len(likes) - 2
+    comments = list(db.comment.find({'playlistId': playlistId_receive}, {'_id': False}))
+
+    print(user_info + "//")
+    print(login)
+
+    return render_template("randomplaylist.html",
+                           playlistId=playlistId_receive,
+                           toptags=top_tags_response,
+                           likes=likes_response,
+                           likes_cnt=likes_cnt,
+                           comments=comments,
+                           user_info=user_info,
+                           login=login)
 
 
 # API 역할을 하는 부분
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    nickname_receive = request.form['nickname_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,  # 아이디
+        "password": password_hash,  # 비밀번호
+        "nickname": nickname_receive,  # 닉네임
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
+
+@app.route('/sign_up/check_dup2', methods=['POST'])
+def check_dup2():
+    nickname_receive = request.form['nickname_give']
+    exists = bool(db.users.find_one({"nickname": nickname_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
+
 @app.route('/search', methods=['GET'])
 def listing():
     query_receive = request.args.get('q')
-    id_receive = "dPMktPKUOfg"
 
     # 키워드 검색 결과 받아오기
     search_response = youtube.search().list(
         q=query_receive,
-        order="date",
+        order="viewCount",
         part="snippet",
-        maxResults=30
+        maxResults=10
     ).execute()
 
-    id_response = youtube.search().list(
-        q=id_receive,
-        part="snippet",
-        maxResults=1
-    ).execute()
-
-    return jsonify({'list': search_response, 'videoId': id_response})
+    return jsonify({'list': search_response})
 
 
 @app.route('/search', methods=['POST'])
 def saving():
     return jsonify({'msg': '저장이 완료되었습니다!'})
+
+
+@app.route('/search_playlist', methods=['GET'])
+def search_playlist():
+    id_receive = request.args.get('id')
+
+    search_response = youtube.playlists().list(
+        id=id_receive,
+        part="snippet"
+    ).execute()
+
+    return jsonify({'list': search_response})
 
 
 @app.route('/tag', methods=['GET'])
